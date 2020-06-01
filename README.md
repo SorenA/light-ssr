609a746767bdb4fc22106cd9c46d725ac86cda85
SorenA <s.dev@lysdal.org>
2020-06-01 18:40:42 +0200

    Add origin whitelist.

--- README.md
+++ README.md
@@ -6,6 +6,20 @@ Inspired by [Rendertron](https://github.com/GoogleChrome/rendertron).
 
 ## Configuration
 
+Configuration is done through environment variables, in development the .env file can be used.
+
+Defaults for environment variables:
+
+```env
+RENDER_TIMEOUT=10000
+RENDER_HEIGHT=1024
+RENDER_WIDTH=1280
+RENDER_ORIGIN_WHITELIST=
+```
+
+`RENDER_TIMEOUT` is render timeout in milliseconds.
+`RENDER_ORIGIN_WHITELIST` should be provided as CSV.
+
 ## Response parsing
 
 The render will return the same HTTP status code as the origin, it may however be overriden using a meta tag:
@@ -21,3 +35,28 @@ JavaScript will be stripped from the response to prevent further processing in t
 The renderer will pass `ssr=true` in the query string to enable different responses for renders.
 
 This may be used to disable cookie pop-ups, trackings scripts etc.
+
+## Security
+
+Due to the security issues created by allowing an open proxy into ones network, the application requires origins to be whitelisted before they are rendered.
+
+All requests not matching the whitelist are rejected.
+
+
+## Development
+
+Create an environment file from the default one:
+
+```bash
+cp .env.example .env
+```
+
+Install dependencies using `npm ci`.
+
+Run local version using `npm run dev`.
+
+### Built with NodeJS packages
+
+- [dotenv](https://github.com/motdotla/dotenv)
+- [Puppeteer](https://github.com/puppeteer/puppeteer)
+- [Koa](https://github.com/koajs/koa)
--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,9 @@ import { RenderWorker } from './render-worker';
  * Configuration - Render
  */
 const renderTimeout = +(process.env.RENDER_TIMEOUT || 10000);
-const renderHeight = +(process.env.RENDER_HEIGHT || 1024 );
+const renderHeight = +(process.env.RENDER_HEIGHT || 1024);
 const renderWidth = +(process.env.RENDER_WIDTH || 1280);
+const renderOriginWhitelist = (process.env.RENDER_ORIGIN_WHITELIST || '').split(',');
 
 const koaHostname = '0.0.0.0';
 const koaPort = 3000;
@@ -67,7 +68,17 @@ app.use(KoaRoute.get('/render', async (ctx: Koa.Context) => {
 
   const url = ctx.query.url;
   const isMobile = 'mobile' in ctx.query ? true : false;
-  const renderResult = await renderWorker.render(url, isMobile);
+
+  const parsedUrl = new URL(url);
+  if (renderOriginWhitelist.indexOf(parsedUrl.host) === -1) {
+    // Origin not whitelisted
+    ctx.status = 400;
+    ctx.body = 'Origin not allowed.';
+    return;
+  }
+
+  // Render url
+    const renderResult = await renderWorker.render(url, isMobile);
 
   // Add result headers, status code and content
   renderResult.headers.forEach((v: string, k: string) => ctx.set(k, v));
@@ -78,4 +89,6 @@ app.use(KoaRoute.get('/render', async (ctx: Koa.Context) => {
 // Start Koa
 app.listen(koaPort, koaHostname, () => {
   console.log(`Started Light-SSR on ${koaHostname}:${koaPort}`);
+  console.log(`Origin whitelist:`);
+  renderOriginWhitelist.forEach((value: string) => console.log(`- ${value}`));
 });
